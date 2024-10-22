a3d7ffdbf1bf01562ab9925a5503350fe73fba46
Hieu Nguyen <hieu.nv2001hust@gmail.com>
2024-10-22 10:27:29 +0700

    2

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,33 +1,34 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import MensajeIngles from './../language/en.json';
 import MensajeEspañol from './../language/es.json';
-import {IntlProvider} from 'react-intl';
+import { IntlProvider } from 'react-intl';
 
 const langContext = React.createContext();
 
+const LangProvider = ({ children }) => {
+    // Thiết lập giá trị mặc định
+    let localePorDefecto = 'en-US'; 
+    let mensajesPorDefecto = MensajeIngles;
 
-const LangProvider = ({children}) => {
-    let localePorDefecto;
-	let mensajesPorDefecto;
-	const lang = localStorage.getItem('lang');
+    // Lấy ngôn ngữ từ localStorage nếu có
+    const lang = localStorage.getItem('lang');
 
-	if(lang){
-		localePorDefecto = lang
+    if (lang) {
+        localePorDefecto = lang;
 
-		if(lang === 'es-ES'){
-			mensajesPorDefecto = MensajeEspañol;
-		} else if(lang === 'en-US'){
-			mensajesPorDefecto = MensajeIngles;
-		} else {
-			localePorDefecto = 'en-US'
-			mensajesPorDefecto = MensajeIngles;
-		}
-	}
+        if (lang === 'es-ES') {
+            mensajesPorDefecto = MensajeEspañol;
+        } else if (lang === 'en-US') {
+            mensajesPorDefecto = MensajeIngles;
+        }
+    }
 
+    // State để lưu trữ ngôn ngữ và messages
     const [mensaje, setMensaje] = useState(mensajesPorDefecto);
     const [locale, setLocale] = useState(localePorDefecto);
 
-    const selectLanguage = (language) =>{
+    // Hàm để chọn ngôn ngữ
+    const selectLanguage = (language) => {
         switch (language) {
             case 'es-ES':
                 setMensaje(MensajeEspañol);
@@ -44,9 +45,11 @@ const LangProvider = ({children}) => {
                 setLocale('en-US');
                 localStorage.setItem('lang', 'en-US');
         }
-    }
+    };
+
     return (
-        <langContext.Provider value={{selectLanguage: selectLanguage}}>
+        <langContext.Provider value={{ selectLanguage }}>
+            {/* IntlProvider sử dụng locale và messages */}
             <IntlProvider locale={locale} messages={mensaje}>
                 {children}
             </IntlProvider>
@@ -54,4 +57,4 @@ const LangProvider = ({children}) => {
     );
 };
 
-export {LangProvider, langContext};
+export { LangProvider, langContext };
